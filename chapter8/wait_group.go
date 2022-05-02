1ef294fbe2432f67cb1bcd4f266e29e1fb4c37a7
songoku <silverchen961@foxmail.com>
2022-05-02 16:39:44 +0800

    add:waitGroup

---
+++ chapter8/wait_group.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1) // 累加计数(原子操作)
+
+		go func(id int) {
+			defer wg.Done() // 递减计数
+			time.Sleep(time.Second)
+			println("goroutine", id, "done.")
+		}(i)
+	}
+
+	println("main...")
+	wg.Wait() // 阻塞，直到计数归零
+	println("main exit...")
+}
