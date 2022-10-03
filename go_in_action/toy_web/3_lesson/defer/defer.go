126af209704cd962d81e8a9c62f3c0a08c778a72
chensh <silverchen961@foxmail.com>
2022-10-03 21:47:39 +0800

    add:defer

---
+++ go_in_action/toy_web/3_lesson/defer/defer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	defer func() {
+		fmt.Println("aaa")
+	}()
+
+	defer func() {
+		fmt.Println("bbb")
+	}()
+
+	defer func() {
+		fmt.Println("ccc")
+	}()
+}
