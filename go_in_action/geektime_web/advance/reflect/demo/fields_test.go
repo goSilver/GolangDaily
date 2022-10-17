c148d63c854042068e07d6fe492b5b8e9ff2a38a
chensh <silverchen961@foxmail.com>
2022-10-17 22:50:41 +0800

    add:reflect

---
+++ go_in_action/geektime_web/advance/reflect/demo/fields_test.go
@@ -0,0 +1,66 @@
+package demo
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type User struct {
+	Name string
+}
+
+func TestIterateFields(t *testing.T) {
+	u1 := &User{
+		Name: "少华",
+	}
+	u2 := &u1
+
+	tests := []struct {
+		name    string
+		val     any
+		wantRes map[string]any
+		wantErr error
+	}{
+		{
+			name:    "nil",
+			val:     nil,
+			wantErr: errors.New("不能为 nil"),
+		},
+		{
+			name:    "user",
+			val:     User{Name: "Tom"},
+			wantErr: nil,
+			wantRes: map[string]any{
+				"Name": "Tom",
+			},
+		},
+		{
+			name:    "pointer",
+			val:     &User{Name: "Jerry"},
+			wantErr: nil,
+			wantRes: map[string]any{
+				"Name": "Jerry",
+			},
+		},
+		{
+			name:    "multiple pointer",
+			val:     u2,
+			wantErr: nil,
+			wantRes: map[string]any{
+				"Name": "少华",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := iterateFields(tt.val)
+			assert.Equal(t, tt.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tt.wantRes, res)
+		})
+	}
+}
---
+++ go_in_action/geektime_web/advance/reflect/demo/reflect_demo_test.go
@@ -0,0 +1,16 @@
+package demo
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestReflectPanic(t *testing.T) {
+	typ := reflect.TypeOf(&User{})
+	if typ.Kind() == reflect.Struct {
+		fmt.Println("结构体")
+	} else if typ.Kind() == reflect.Ptr {
+		fmt.Println("指针")
+	}
+}
