e3b8f87788106c55ea9221931f82088df4df1ddb
chensh <silverchen961@foxmail.com>
2022-10-30 21:51:59 +0800

    add:context处理输入输出

---
+++ go_in_action/geektime_web/web/v5/context.go
@@ -0,0 +1,89 @@
+package v5
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type Context struct {
+	Req        *http.Request
+	Resp       http.ResponseWriter
+	PathParams map[string]string
+
+	// 缓存的数据
+	cacheQueryValues url.Values
+}
+
+func (c *Context) BindJSON(val any) error {
+	if c.Req.Body == nil {
+		return errors.New("web: body为nil")
+	}
+	decoder := json.NewDecoder(c.Req.Body)
+	decoder.DisallowUnknownFields()
+	return decoder.Decode(val)
+}
+
+func (c *Context) FormValue(key string) StringValue {
+	// 需要先使用ParseForm解析表单数据，ParseForm是幂等的。
+	if err := c.Req.ParseForm(); err != nil {
+		return StringValue{err: err}
+	}
+	return StringValue{val: c.Req.FormValue(key)}
+}
+
+func (c *Context) QueryValue(key string) StringValue {
+	if c.cacheQueryValues == nil {
+		c.cacheQueryValues = c.Req.URL.Query()
+	}
+	vals, ok := c.cacheQueryValues[key]
+	if !ok {
+		return StringValue{err: errors.New("web: 找不到这个 key")}
+	}
+	return StringValue{val: vals[0]}
+}
+
+func (c *Context) PathValue(key string) StringValue {
+	val, ok := c.PathParams[key]
+	if !ok {
+		return StringValue{err: errors.New("web: 找不到这个 key")}
+	}
+	return StringValue{val: val}
+}
+
+func (c *Context) SetCookie(cookie *http.Cookie) {
+	http.SetCookie(c.Resp, cookie)
+}
+
+func (c *Context) RespOkJSON(code int, val any) error {
+	bs, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	c.Resp.WriteHeader(code)
+	_, err = c.Resp.Write(bs)
+	return err
+}
+
+type StringValue struct {
+	val string
+	err error
+}
+
+func (s StringValue) String() (string, error) {
+	return s.val, s.err
+}
+
+func (s StringValue) ToInt64() (int64, error) {
+	if s.err != nil {
+		return 0, s.err
+	}
+	return strconv.ParseInt(s.val, 10, 64)
+}
+
+// 不能用泛型
+// func (s StringValue) To[T any]() (T, error) {
+//
+// }
