cd8b193ab65334d0b2da75c96513323c0cad619c
chensh <silverchen961@foxmail.com>
2023-02-06 22:59:41 +0800

    add:简单工厂

---
+++ design_pattern/b_factory/simple_factory.go
@@ -0,0 +1,36 @@
+package b_factory
+
+// ConfigParse 配置解析接口
+type ConfigParse interface {
+	Parse(path string) string
+}
+
+// JsonConfigParse Json解析类
+type JsonConfigParse struct {
+}
+
+func (json *JsonConfigParse) Parse(path string) string {
+	return "Json" + path
+}
+
+// XmlConfigParse xml解析类
+type XmlConfigParse struct {
+}
+
+func (xml *XmlConfigParse) Parse(path string) string {
+	return "XML" + path
+}
+
+// SimpleParseFactory 简单工厂
+type SimpleParseFactory struct {
+}
+
+func (simple *SimpleParseFactory) create(ext string) ConfigParse {
+	switch ext {
+	case "json":
+		return &JsonConfigParse{}
+	case "xml":
+		return &XmlConfigParse{}
+	}
+	return nil
+}
